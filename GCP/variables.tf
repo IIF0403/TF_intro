4863e088690ca61f52557a3fd960a7342e718e96
IIF0403 <70519691+IIF0403@users.noreply.github.com>
2021-08-16 09:11:36 +0200

    Update variables.tf

--- GCP/variables.tf
+++ GCP/variables.tf
@@ -1,6 +1,5 @@
 variable "cred_file" {
-  default = "poetic-maxim-323015-a960c24099a3.json"
-  #default = "C:/Users/ifougner/GCP-testing/poetic-maxim-323015-8ab95c8e777a.json"
+  default = {}
 }
 
 variable "region" {
